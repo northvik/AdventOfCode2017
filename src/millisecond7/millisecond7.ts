f4b922b33e159905c35d87ef08bd5863c74e97be
Camille Pire <c.pire@elsevier.com>
2018-04-12 14:53:24 +0100

    millisecond 7

--- src/millisecond7/millisecond7.ts
+++ src/millisecond7/millisecond7.ts
@@ -2,45 +2,115 @@ import * as _ from "lodash";
 import { IMillisecond, InputArray } from '../../Interface';
 import { BaseMillisecond } from '../baseMillisecond';
 
+export interface ITower {
+    name: string;
+    weight: number;
+    totalWeight: number,
+    onDiscNames: string[],
+    onDisc: ITower[];
+}
+
 export class Millisecond7 extends BaseMillisecond implements IMillisecond {
-    private towers;
+    private towers: ITower[];
+    private towerTree: ITower;
 
     public solvePartOne(input: InputArray): any {
         this.towers = [];
         this.parseInput(input);
-        return this.getFirstTower();
+        return this.getFirstTowerName();
     }
 
     public solvePartTwo(input: InputArray): any {
-        return 'Not Solved ' + input[0];
+        this.towers = [];
+        this.parseInput(input);
+        this.getTree();
+        return this.getWeightAnomaly(this.towerTree);
     }
 
     protected parseInput(input: InputArray): void {
         const regex = new RegExp(/([a-z]+) \(([0-9]+)\)( ->([a-z ,]+))?/);
         for (const row of input) {
             let result = regex.exec(row);
-            let onDisc = [];
+            let onDiscNames = [];
             if (typeof result[4] === 'string' && result[4].length > 1) {
-                onDisc = result[4].replace(/ /g, '').split(',');
+                onDiscNames = result[4].replace(/ /g, '').split(',');
             }
             this.towers.push({
                 name: result[1],
-                weight: result[2],
-                onDisc: onDisc
+                weight: parseInt(result[2], 10),
+                totalWeight: parseInt(result[2], 10),
+                onDiscNames: onDiscNames,
+                onDisc: []
             });
         }
     }
 
-    protected getFirstTower(): string {
-        let onDisc = [];
+    protected getFirstTowerName(): string {
+        let onDiscNames = [];
         let names = [];
         for (let tower of this.towers) {
             names.push(tower.name);
-            for (let onDTower of tower.onDisc) {
-                onDisc.push(onDTower);
+            for (let onDTower of tower.onDiscNames) {
+                onDiscNames.push(onDTower);
             }
         }
-        const result = _.difference(names, _.uniq(onDisc));
+        const result = _.difference(names, _.uniq(onDiscNames));
         return result[0];
+    }
+
+    protected getTree() {
+        const firstTower = _.find(this.towers, {name: this.getFirstTowerName()});
+        this.towerTree = this.buildTree(firstTower);
+    }
+
+    protected buildTree(tower: ITower): ITower {
+        if (tower.onDiscNames){
+            for (let towerName of tower.onDiscNames) {
+                let findTower = _.find(this.towers, {name: towerName});
+                if (findTower){
+                    tower.onDisc.push(this.buildTree(findTower));
+                    tower.totalWeight += findTower.totalWeight;
+                }
+            }
+        }
+        return tower;
+    }
+
+    protected getWeightAnomaly(tower: ITower) {
+        let subTowerTotal = [];
+        for (let subTower of tower.onDisc) {
+            subTowerTotal.push(subTower.totalWeight);
+        }
+        if (_.uniq(subTowerTotal).length > 1){
+            const min = this.getMinority(subTowerTotal);
+            const maj = this.getMajority(subTowerTotal);
+
+            for (let subTower of tower.onDisc) {
+                if (subTower.totalWeight === min){
+                    if (subTower.onDisc.length > 0){
+                        let number = this.getWeightAnomaly(subTower);
+                        if (number) {
+                            return number
+                        }
+                    }
+                    return subTower.weight - (min - maj);
+                }
+            }
+        }
+        return false;
+    }
+
+    protected getMajority(arr){
+        return arr.sort((a,b) =>
+            arr.filter(v => v===a).length
+            - arr.filter(v => v===b).length
+        ).pop();
+    }
+
+    protected getMinority(arr){
+        return arr.sort((a,b) =>
+            arr.filter(v => v===b).length
+            - arr.filter(v => v===a).length
+        ).pop();
     }
 }
--- test/millisecond7.test.ts
+++ test/millisecond7.test.ts
@@ -19,15 +19,34 @@ const input = [
         'cntj (57)'
     ];
 
+const input2 = [
+    'pbga (66)',
+    'xhth (57)',
+    'ebii (61)',
+    'havc (66)',
+    'ktlj (57)',
+    'fwft (3) -> ktlj, cntj, xhth',
+    'qoyq (66)',
+    'padx (45) -> pbga, havc, qoyq',
+    'tknk (41) -> ugml, padx, fwft',
+    'jptl (61)',
+    'ugml (60) -> gyxo, ebii, jptl',
+    'gyxo (61)',
+    'cntj (57)'
+];
+
 describe('Testing Millisecond 7', () => {
     describe('Part 1', () => {
         it('should return 5 steps', () => {
             expect(millisecond.solvePartOne(input)).to.equal('tknk');
         });
     });
-    // describe('Part 2', () => {
-    //     it('should return 4 steps', () => {
-    //         expect(millisecond.solvePartTwo(input)).to.equal(4);
-    //     });
-    // });
+    describe('Part 2', () => {
+        it('should return weight 60', () => {
+            expect(millisecond.solvePartTwo(input)).to.equal(60);
+        });
+        it('should return weight 72', () => {
+            expect(millisecond.solvePartTwo(input2)).to.equal(72);
+        });
+    });
 });
