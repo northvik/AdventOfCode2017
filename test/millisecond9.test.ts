759ae096c4cddff90ea5e68f6535fc81453c6b29
Camille Pire <c.pire@elsevier.com>
2018-04-18 10:27:00 +0100

    Adding millisecond 9

--- test/millisecond9.test.ts
+++ test/millisecond9.test.ts
@@ -30,9 +30,18 @@ describe('Testing Millisecond 9', () => {
             expect(millisecond.solvePartOne(['{{<a!>},{<a!>},{<a!>},{<ab>}}'])).to.equal(3);
         });
     });
-    // describe('Part 2', () => {
-    //     it('should return highest ever number 10', () => {
-    //         expect(millisecond.solvePartTwo(input)).to.equal(10);
-    //     });
-    // });
+    describe('Part 2', () => {
+        it('should return 0 garbage', () => {
+            expect(millisecond.solvePartTwo(['<>'])).to.equal(0);
+        });
+        it('should return 17 garbage', () => {
+            expect(millisecond.solvePartTwo(['<random characters>'])).to.equal(17);
+        });
+        it('should return 3 garbage', () => {
+            expect(millisecond.solvePartTwo(['<<<<>'])).to.equal(3);
+        });
+        it('should return 10 garbage', () => {
+            expect(millisecond.solvePartTwo(['<{o"i!a,<{i<a>'])).to.equal(10);
+        });
+    });
 });
